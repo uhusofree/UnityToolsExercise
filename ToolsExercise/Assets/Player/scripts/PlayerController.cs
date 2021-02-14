ea18f4e61d80c7716ebfc582babb4ea61c233a3c
FGWH18033\uhuru.mccray <uhusofree@github.com>
2021-02-14 12:03:54 +0100

    Camera script,  Truck Movement

---
+++ ToolsExercise/Assets/Player/scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Assertions;
+using UnityEngine;
+
+namespace UnityTools.Player
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerController : MonoBehaviour
+    {
+        public float movementSpeed;
+        public float rotationSpeed;
+
+        // Start is called before the first frame update
+
+        // Update is called once per frame
+        void Update()
+        {
+            movementSpeed = Input.GetAxis("Horizontal") * Time.deltaTime;
+            rotationSpeed = Input.GetAxis("Vertical") * Time.deltaTime;
+
+            transform.Translate(0, 0, movementSpeed);
+            transform.Rotate(0, rotationSpeed, 0); 
+        }
+    }
+
+}
---
+++ ToolsExercise/Assets/_Camera/_scripts/TopDown_Camera.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityTools.Cam
+
+{
+    public class TopDown_Camera : MonoBehaviour
+    {
+        public Transform target;
+        public float height = 10f;
+        public float dist = 20f;
+        public float angle = 45f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            HandleCam();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            HandleCam();
+        }
+
+        private void HandleCam() 
+        {
+            if (target == null)
+            {
+                return;
+            }
+            Vector3 wPosition = (Vector3.forward * -dist) + (Vector3.up * height);
+
+            Vector3 rotatedVec = Quaternion.AngleAxis(angle, Vector3.up) * wPosition;
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.green; 
+            Gizmos.DrawLine(target.position, (Vector3.forward * -dist) + (Vector3.up * height));
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(target.position, Quaternion.AngleAxis(angle, Vector3.up) 
+                * (Vector3.forward * -dist) + (Vector3.up * height));
+        }
+    }
+}
