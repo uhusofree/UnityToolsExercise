c464c6b2f392ac8f9688be2fefdfbf22e0e5c2f2
FGWH18033\uhuru.mccray <uhusofree@github.com>
2021-02-26 16:26:43 +0100

    enemy positon and rotation v.1

---
+++ ToolsExercise/Assets/NPC/scripts/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Movement : MonoBehaviour
+{
+    
+
+
+    [SerializeField] float speed = 10f;
+    [SerializeField] float tiltAngle = 50f;
+    [SerializeField] float smooth = 10f;
+    
+    Vector3 targetPos;
+    Position pos;
+
+    private void Start()
+    {
+        targetPos = pos.GetPosition();
+    }
+
+    private void Update()
+    {
+        //move to targeting function
+        float turnX = targetPos.x * tiltAngle;
+        float turnZ = targetPos.z * tiltAngle;
+        Quaternion target = Quaternion.Euler(turnX, 0, turnZ);
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, smooth * Time.deltaTime);
+
+        transform.position += Vector3.forward  * speed * Time.deltaTime;
+        //transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+    }
+
+
+}
---
+++ ToolsExercise/Assets/Player/scripts/Position.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Position : MonoBehaviour
+{
+    public Vector3 GetPosition()
+    {
+        return this.transform.position;
+    }
+}
